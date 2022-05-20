72d054cdd6b00eca76581b1c4ad50a9dd5f383a6
Nevul <alexmaxter1232@gmail.com>
2022-05-19 21:24:16 -0500

    Cambios Plataforma 19-05-2022
    Ajustes en movimiento de plataformas

--- Assets/Script/SpaceYue/CameraFollow.cs
+++ Assets/Script/SpaceYue/CameraFollow.cs
@@ -24,7 +24,7 @@ public class CameraFollow : MonoBehaviour
     }
 
     void MoveCamera(bool smooth){
-        offset = new Vector3(-2.35f, 2.0f, -10f);
+        offset = new Vector3(-2.35f, 0.5f, -10f);
         Vector3 destination = new Vector3(
                             target.position.x - offset.x,
                             target.position.y - offset.y,
