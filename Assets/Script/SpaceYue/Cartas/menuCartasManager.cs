cbe1e5aaa5d2f4a1544b3a61c8cb76941c4b2792
Nevul <alexmaxter1232@gmail.com>
2022-05-27 02:29:15 -0500

    Win y Lose

--- Assets/Script/SpaceYue/Cartas/menuCartasManager.cs
+++ Assets/Script/SpaceYue/Cartas/menuCartasManager.cs
@@ -1,9 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class menuCartasManager : MonoBehaviour
 {
+    [SerializeField] Text ScoreWin;
+    [SerializeField] Text ScoreLose;
     public static menuCartasManager sharedInstance;
 
     private void Awake()
@@ -23,13 +26,14 @@ public class menuCartasManager : MonoBehaviour
     public void ShowVictory(int score)
     {
         GameManager.shareInstance.WinGame();
-        EstatusCartas.sharedInstance.WinPoints(score);
+
+        WinPoints(score);
     }
 
     public void ShowDefeat(int score)
     {
         GameManager.shareInstance.GameOver();
-        EstatusCartas.sharedInstance.LosePoints(score);
+        LosePoints(score);
     }
     public void NoShowVictory()
     {
@@ -39,5 +43,13 @@ public class menuCartasManager : MonoBehaviour
     public void NoShowDefeat()
     {
         menuCanvas[1].enabled = false;
+    }
+    public void WinPoints(int score)
+    {
+        ScoreWin.text = score.ToString() + " Pts.";
+    }
+    public void LosePoints(int score)
+    {
+        ScoreLose.text = score.ToString() + " Pts.";
     }
 }
