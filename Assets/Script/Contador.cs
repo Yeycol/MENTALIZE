65f1452354bc1955d49a7f97f1630805e83b318c
Yeycol Clerque <yeycolcl@gmail.com>
2022-07-23 22:41:11 -0500

    Cambios Ramdom

--- Assets/Script/Contador.cs
+++ Assets/Script/Contador.cs
@@ -51,11 +51,12 @@ public class Contador : MonoBehaviour
             sharecont = this;
         }
         scene = SceneManager.GetActiveScene();//GetActiveScene es un m�todo que nos permite obtener la escena activa actualmente
-        
+        ResetSound();//Se llama al m�todo encargado de parar los sonidos para que ninguno de la anterior escena sea escuchado
     }
     void Start()
     {
-        if (GameManager.shareInstance.currentgameState == GameState.InGame) ResetSound();//Se llama al m�todo encargado de parar los sonidos para que ninguno de la anterior escena sea escuchado
+        Contador.sharecont.controlCoinsYue = 0;
+        PlayerPrefs.SetInt("controlCoinsYue", Contador.sharecont.controlCoinsYue);
         /*Solo usar en caso de querer resetear los valores almacenados en los key de los player prefs
         PlayerPrefs.DeleteKey("ActivarEvento");
         PlayerPrefs.DeleteKey("ExtraMoneda");
